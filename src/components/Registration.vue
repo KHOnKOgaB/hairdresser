20858ec833be9ae6839c5dce682f670a7a7e0d62
Dmitriy <d.g.kovalev@gmail.com>
2020-03-20 15:00:21 +0300

    aos & fontawesome

--- src/components/Registration.vue
+++ src/components/Registration.vue
@@ -32,7 +32,6 @@
 </template>
 
 <script>
-
 </script>
 
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue';
+import VueAos from 'vue-aos';
+import BootstrapVue from 'bootstrap-vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faHandPointDown } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import App from './App.vue';
-
+library.add(faHandPointDown);
 Vue.config.productionTip = false;
-
+Vue.use(BootstrapVue);
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 new Vue({
   render: (h) => h(App),
 }).$mount('#app');
