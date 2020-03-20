aeb6be0352d681c019e419bcaade48464d1236ef
Ruslan Vasiliev <ruzero.io@gmail.com>
2020-03-21 00:32:48 +0200

    responsive form

--- src/components/Registration.vue
+++ src/components/Registration.vue
@@ -7,17 +7,10 @@
     ref="form"
     action="https://docs.google.com/forms/u/0/d/e/1FAIpQLSfXix3XzMPKy4dBE9z_4CvSmCQi4B6w8uT_ahxCnkCzpCWUFg/formResponse">
     <h2 class="font-weight-bolder text-center">Регистрация на марафон</h2>
-<<<<<<< HEAD
     <div class="align-items-center justify-content-around">
       <b-container class="py-5">
         <b-row class="mb-5">
           <b-col md="6" class="mb-4">
-=======
-    <div class="row align-items-center justify-content-around">
-      <div class="register py-5">
-        <div class="row mb-5">
-          <div class="col-12 col-md-6">
->>>>>>> 627bdba86a4ecc72813b01088d9d9bf0e8ba19bc
             <b-form-input
               v-model="form.name"
               required
@@ -25,13 +18,8 @@
               name="entry.1880855526"
               class="form-control"
               placeholder="Введите ваше имя"/>
-<<<<<<< HEAD
           </b-col>
           <b-col md="6" class="mb-4">
-=======
-          </div>
-          <div class="col-12 col-md-6">
->>>>>>> 627bdba86a4ecc72813b01088d9d9bf0e8ba19bc
             <b-form-input
               v-model="form.email"
               required
@@ -39,15 +27,8 @@
               name="entry.1562501161"
               class="form-control"
               placeholder="Введите ваш email"/>
-<<<<<<< HEAD
           </b-col>
           <b-col md="6" class="mb-4">
-=======
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-12 col-md-6">
->>>>>>> 627bdba86a4ecc72813b01088d9d9bf0e8ba19bc
             <b-form-input
               v-model="form.phone"
               required
@@ -55,13 +36,8 @@
               class="form-control"
               name="entry.666901678"
               placeholder="Введите ваш телефон"/>
-<<<<<<< HEAD
           </b-col>
           <b-col md="6" class="mb-4">
-=======
-          </div>
-          <div class="col-12 col-md-6">
->>>>>>> 627bdba86a4ecc72813b01088d9d9bf0e8ba19bc
             <b-form-input
               v-model="form.instagram"
               required
@@ -74,13 +50,8 @@
         </b-row>
       </b-container>
     </div>
-<<<<<<< HEAD
     <h2 class="font-weight-bolder text-center text-white">Стоимость участия 199₽</h2>
     <div class="my-3 text-white">
-=======
-    <h2 class="font-weight-bolder text-center text-white mt-5">Стоимость участия 199₽</h2>
-    <div class="my-4 text-white">
->>>>>>> 627bdba86a4ecc72813b01088d9d9bf0e8ba19bc
       <font-awesome-icon class="hand-down" icon="hand-point-down" />
       <font-awesome-icon class="hand-down ml-4" icon="hand-point-down" />
       <font-awesome-icon class="hand-down ml-4" icon="hand-point-down" />
