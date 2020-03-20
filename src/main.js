f90fb30f5cfbbf54c881db209a72fae21c49a928
Dmitriy <d.g.kovalev@gmail.com>
2020-03-20 15:15:15 +0300

    aos

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 import Vue from 'vue';
-import VueAos from 'vue-aos';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import BootstrapVue from 'bootstrap-vue';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faHandPointDown } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import App from './App.vue';
+
 library.add(faHandPointDown);
+
+AOS.init();
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
 Vue.component('font-awesome-icon', FontAwesomeIcon);
