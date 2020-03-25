584142e50f7a2b92df1287636e6dc0ae3dc23086
Ruslan Vasiliev <ruzero.io@gmail.com>
2020-03-25 18:03:01 +0200

    info

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import BootstrapVue from 'bootstrap-vue';
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faHandPointDown, faInfo } from '@fortawesome/free-solid-svg-icons';
 import { faInstagram } from '@fortawesome/free-brands-svg-icons';
@@ -15,6 +15,7 @@ library.add(faHandPointDown, faInstagram, faInfo);
 AOS.init();
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
+Vue.use(BootstrapVueIcons);
 Vue.use(vueSmoothScroll);
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 Vue.use(VueTheMask);
